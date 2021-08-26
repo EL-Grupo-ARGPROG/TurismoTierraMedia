e97fee0fb67f0fe9a4321b0c3e6b7f54368904db
Matias Ayala <matias.cayala@gmail.com>
2021-08-26 18:40:19 -0300

    calcularCosto y tiempo v1

--- src/TierraMedia/Absoluta.java
+++ src/TierraMedia/Absoluta.java
@@ -2,12 +2,27 @@ package TierraMedia;
 
 public class Absoluta extends Promociones {
 	
+	private final int DESCUENTO;
+	
+	public Absoluta(Atracciones[] pack, String nombre, int descuento) {
+		this.pack = pack;
+		this.nombre = nombre;
+		this.DESCUENTO = descuento;
+	}
+
+	
 	public int calcularCosto() {
-		return 0;
+		for (Atracciones atraccion : this.pack) {
+			this.costoPromocion += atraccion.getCosto();
+		}
+		return this.costoPromocion - DESCUENTO;
 	}
 	
 	public int calcularTiempo() {
-		return 0;
+		for (Atracciones atraccion : this.pack) {
+			this.tiempoNecesario += atraccion.getTiempoNecesario();
+		}
+		return this.tiempoNecesario;
 	}
 
 }
