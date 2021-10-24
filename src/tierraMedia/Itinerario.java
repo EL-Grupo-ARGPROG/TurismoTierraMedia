2377dd2da1e5f5c163865ef08b2a4587f622b99c
feltom1000 <feltom1000@hotmail.com>
2021-10-24 15:34:28 -0300

    Feat: Added clase Itinerario, Added ItinerarioDAOImpl

---
+++ src/tierraMedia/Itinerario.java
@@ -0,0 +1,42 @@
+package tierraMedia;
+
+public class Itinerario {
+	int id;
+	int id_usuario;
+	Vendible atraccion;
+	Vendible promocion;
+	
+	public Itinerario(int id, int id_usuario, Vendible atraccion, Vendible promocion) {
+		this.id = id;
+		this.id_usuario = id_usuario;
+		this.atraccion = atraccion;
+		this.promocion = promocion;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+	
+	public Vendible getAtraccion() {
+		return atraccion;
+	}
+	public void setAtraccion(Atracciones atraccion) {
+		this.atraccion = atraccion;
+	}
+	
+	public Vendible getPromocion() {
+		return promocion;
+	}
+	public void setPromocion(Promociones promocion) {
+		this.promocion = promocion;
+	}
+
+}
